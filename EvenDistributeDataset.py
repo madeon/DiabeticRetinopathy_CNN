4450a0546d0870001206c867f893b5c8ed838f3a
Mathias <mathias-1992@live.dk>
2018-10-29 18:42:03 +0100

    Updated comments in the file

--- EvenDistributeDataset.py
+++ EvenDistributeDataset.py
@@ -1,3 +1,17 @@
+# This file is used in the pre-processing of the dataset.
+# Each image is moved into a new folder based on what label it is connected to in the trainLabels.csv file.
+
+# Example:
+
+# An image with the label 1 will be moved to a new folder called 1
+# After every image has been moved, 5 new folders have been created: 0, 1, 2, 3 and 4 with all of the 35.126 images in them.
+
+# 0 contains: 25.810 images
+# 1 contains: 2443 images
+# 2 contains: 5292 images
+# 3 contains: 873 images
+# 4 contains: 708 images
+
 
 from PIL import Image
 import pandas as pd
