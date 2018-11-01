9971d4676559975150b9d6c2be6ad5f7ddb8cc49
Mathias <mathias-1992@live.dk>
2018-11-01 01:16:51 +0100

    Renamed some stuff

--- Resize.py
+++ Resize.py
@@ -18,11 +18,9 @@ def resize(current_path, new_path):
     dirs = [l for l in os.listdir(current_path) if l != '.DS_Store']
     total = 0
 
-    # Opens each image in the directory and performs crop on each image
+    # Opens each image in the directory and performs resize on each image
     for item in dirs:
         image = Image.open(current_path + item)
-
-
         print("Saving: ", item, total)
         resized_image = image.resize([image_size, image_size], Image.ANTIALIAS)
         resized_image.save(os.path.join(new_path, item), 'JPEG')
